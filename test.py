d8338df3f79821ed1bb0cd352099c7ff4d22d257
Code1301 <trickymind1301@gmail.com>
2018-09-24 11:42:49 +0200

    Revert "more changes"
    This reverts commit 55daba54a2b2cbb00390e34434a45992c8d1b50c.

--- test.py
+++ test.py
@@ -2,6 +2,5 @@ changes = input("Is this file has been changes?")
 if changes == "yes":
     print 'then commit it'
     print 'changes commit and undo'
-    print 'more changes'
 else:
     print 'then change it'
