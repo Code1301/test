55daba54a2b2cbb00390e34434a45992c8d1b50c
Code1301 <trickymind1301@gmail.com>
2018-09-24 11:40:29 +0200

    more changes

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ changes = input("Is this file has been changes?")
 if changes == "yes":
     print 'then commit it'
     print 'changes commit and undo'
+    print 'more changes'
 else:
     print 'then change it'
